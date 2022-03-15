08359cc2bbc80637d93c8e6966424eb3dfccba4a
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-03-14 23:06:04 -0300

    api get maps

--- src/pages/mapas/index.tsx
+++ src/pages/mapas/index.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
 import Head from "next/head";
+import api from '../../lib/api';
 
 export default function Mapas() {
+  const [ maps, setMaps ] = useState([]);
+
+  useEffect(() => {
+    api.get('maps')
+      .then((response) => {
+        setMaps(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((response) => {
+        console.error(response.error);
+      });
+  }, []);
+
   return (
     <>
       <Head>
