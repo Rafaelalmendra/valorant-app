559903ee1f424d0df32048e28e74a75c6f9bef2d
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-12-14 10:40:03 -0300

    feat: add hootjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -21,6 +22,21 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+      <Script
+        id="hotjar"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3288324,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+        }}
+      />
       <GlobalStyle />
       <Navbar />
       <NavbarMobile />
